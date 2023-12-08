bb5a651091c16c326f00d2fb6ded3f6b5b01135c
Sagar-Patil-C94 <sagarp@ivoyant.com>
2023-12-08 15:39:45 +0530

    CustomExecption

--- src/OOPS/ExceptionHandling.java
+++ src/OOPS/ExceptionHandling.java
@@ -1,6 +1,13 @@
 package OOPS;
 
 // Exception Handling In Java = try, catch, throw, throws, finally
+
+class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
+
 public class ExceptionHandling {
     public static void main(String[] args) {
         int a = 10, b = 0;
@@ -11,8 +18,9 @@ public class ExceptionHandling {
         } finally {
             System.out.println("End Of The Code");
         }
-        func();
+//        func();
         System.out.println("End The Program!!");
+        customExceptionfunc();
     }
 
     // Code that may throw CustomException
@@ -20,6 +28,15 @@ public class ExceptionHandling {
         int a = 10, b = 0;
         if (b == 0) {
             throw new ArithmeticException("Divide By Zero!!");
+        } else {
+            System.out.println(a / b);
+        }
+    }
+
+    public static void customExceptionfunc() {
+        int a = 10, b = 0;
+        if (b == 0) {
+            throw new CustomException("Divide By Zero!!");
         } else {
             System.out.println(a / b);
         }
